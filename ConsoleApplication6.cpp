9da81500cbfdea4bcdf71383f79bfcbf590f5402
jaminka <jaminurmatoc04@gmail.com>
2024-02-09 16:06:58 +0545

    Добавьте файлы проекта.

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,16 @@
+﻿# include <iostream>
+
+void counting_function(int& num);
+
+int main(int argc, char** argv){
+    int count = 0;
+    for (int i = 0; i < 15; i++)
+    {
+        counting_function(count);
+    }
+}
+
+void counting_function(int &num) {
+    num++;
+    std::cout << "quantity of functions call counting_function(): " << num << std::endl;
+}
